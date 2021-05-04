f4495ef931a35e92f0fc0788643ef0c0ac310cb3
Akshay A <adominic21@gmail.com>
2021-05-04 09:36:04 +0530

    added Product componet

---
+++ src/app/commons/product/Product.ts
@@ -0,0 +1,12 @@
+
+export class Products{
+    sku:String;
+    name:String;
+    description:String;
+    unitPrice:number;
+    imageUrl:String;
+    active:boolean;
+    unitsinStock:number;
+    dateCreated:Date;
+    lastUpdated:Date;
+}
